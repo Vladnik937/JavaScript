edabc9df903fc508c19e0f052837293dc07f7a3f
Vladislav949 <crazqx949@gmail.com>
2024-11-06 21:19:11 +0300

    js_hw_1_1

---
+++ JS_HW_1_1.js
@@ -0,0 +1,15 @@
+const check_age = function(age) {
+    if(age < 10) {
+        return "You don't have access cause your age is " + age + " It's less then ";
+    } else if(age >= 18 && age < 60) {
+        return "Welcome !";
+    } else if(age >= 60) {
+        return "Keep calm and look Culture channel";
+    } else {
+        return "Technical work";
+    }
+}
+
+console.log(check_age(17));
+console.log(check_age(18));
+console.log(check_age(61));
